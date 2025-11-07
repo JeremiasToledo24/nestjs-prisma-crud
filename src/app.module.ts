9136c7818c4f716670cacce106dbbe6f7cd0a410
JeremiasToledo24 <jeremias.toledo@outlook.com>
2025-11-06 21:33:09 -0300

    implementado monitoreo de apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { ChatModule } from './chat/chat.module';
 import { MessageModule } from './message/message.module';
 import { AppController } from './app.controller';
 import { MetricsModule } from './metrics/metrics.module';
+// --- NUEVOS IMPORTS ---
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MetricsInterceptor } from './metrics/metrics.interceptor';
+// --- FIN NUEVOS IMPORTS ---
 @Module({
   imports: [ChatModule, MessageModule, MetricsModule],
   controllers: [AppController],
-  providers: [],
+  providers: [{
+      provide: APP_INTERCEPTOR,
+      useClass: MetricsInterceptor,
+    }],
 })
 export class AppModule {}
